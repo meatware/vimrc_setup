7f5a8d33f02a509d178df37c2eb63af0f584f25d
meatware <wetware@zoho.com>
2018-09-21 19:11:57 +0530

    correct .vimrc symlink

--- install_vim_settings.sh
+++ install_vim_settings.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-cd ~
 #git clone https://github.com/meatware/vimrc_setup.git ~/.vim
-ln -s ~/.vim/vimrc ~/.vimrc
+rm ~/.vimrc
+ln -s ~/.vim/.vimrc ~/.vimrc
 #ln -s ~/.vim/gvimrc ~/.gvimrc
 
 cd ~/.vim
