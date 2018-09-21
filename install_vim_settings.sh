7411aabda1119af5229e2c5ab2eca5f09b37c7bc
meatware <wetware@zoho.com>
2018-09-21 18:29:28 +0530

    added .gitmodules

--- install_vim_settings.sh
+++ install_vim_settings.sh
@@ -2,11 +2,12 @@
 
 
 cd ~
-git clone https://github.com/meatware/vimrc_setup.git ~/.vim
+#git clone https://github.com/meatware/vimrc_setup.git ~/.vim
 ln -s ~/.vim/vimrc ~/.vimrc
 #ln -s ~/.vim/gvimrc ~/.gvimrc
 
 cd ~/.vim
 git submodule init
 git submodule update
+
 exit 0
