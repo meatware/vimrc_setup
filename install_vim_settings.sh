cd78909bcbf91fba2c3db2fcc1293fa830d6fb7d
meatware <wetware@zoho.com>
2018-09-21 12:28:16 +0530

    added help-ref and script to install vim settings on new machines

---
+++ install_vim_settings.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+cd ~
+git clone https://github.com/meatware/vimrc_setup.git ~/.vim
+ln -s ~/.vim/vimrc ~/.vimrc
+#ln -s ~/.vim/gvimrc ~/.gvimrc
+
+cd ~/.vim
+git submodule init
+git submodule update
+exit 0
